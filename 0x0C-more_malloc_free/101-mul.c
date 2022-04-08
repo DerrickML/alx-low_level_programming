b3c047ef0711a0bf7f36230de5dca900158a6b65
DeroMal <derrickmal123@gmail.com>
2022-04-08 03:41:17 +0300

    what a hard one

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,126 +1,30 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
-
+#include <stdlib.h>
+#include "main.h"
 /**
- * _is_zero - determines if any number is zero
- * @argv: argument vector.
- *
- * Return: no return.
- */
-void _is_zero(char *argv[])
-{
-  int i, num1 = 1, num2 = 1;
-
-  for (i = 0; argv[1][i]; i++)
-    if (argv[1][i] != '0')
-    {
-      num1 = 0;
-      break;
-    }
-
-  for (i = 0; argv[2][i]; i++)
-    if (argv[2][i] != '0')
-    {
-      num2 = 0;
-      break;
-    }
-
-  if (num1 == 1 || num2 == 1)
-  {
-    printf("0\n");
-    exit(0);
-  }
-}
-
-/**
- * _initialize_array - set memery to zero in a new array
- * @ar: char array.
- * @lar: length of the char array.
- *
- * Return: pointer of a char array.
- */
-char *_initialize_array(char *ar, int lar)
-{
-  int i = 0;
-
-  for (i = 0; i < lar; i++)
-    ar[i] = '0';
-  ar[lar] = '\0';
-  return (ar);
-}
-
-/**
- * _checknum - determines length of the number
- * and checks if number is in base 10.
- * @argv: arguments vector.
- * @n: row of the array.
- *
- * Return: length of the number.
- */
-int _checknum(char *argv[], int n)
-{
-  int ln;
-
-  for (ln = 0; argv[n][ln]; ln++)
-    if (!isdigit(argv[n][ln]))
-    {
-      printf("Error\n");
-      exit(98);
-    }
-
-  return (ln);
-}
-
-/**
- * main - Entry point.
- * program that multiplies two positive numbers.
- * @argc: number of arguments.
- * @argv: arguments vector.
- *
- * Return: 0 - success.
- */
+* main - multiply 2 numbers as arguments
+* @argc: number of arguments
+* @argv: arguments
+* Return: 0 success
+*/
 int main(int argc, char *argv[])
 {
-  int ln1, ln2, lnout, add, addl, i, j, k, ca;
-  char *mul;
+unsigned long mul;
+int num1, num2;
+	if (argc != 3)
+	{ printf("Error\n");
+	exit(98); }
+	for (num1 = 1; num1 < argc; num1++)
+	{
+		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
+		{
+			if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
+			{  printf("Error\n");
+			exit(98); }
+		}
 
-  if (argc != 3)
-    printf("Error\n"), exit(98);
-  ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
-  _is_zero(argv), lnout = ln1 + ln2, mul = malloc(lnout + 1);
-  if (mul == NULL)
-    printf("Error\n"), exit(98);
-  mul = _initialize_array(mul, lnout);
-  k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-  for (; k >= 0; k--, i--)
-  {
-    if (i < 0)
-    {
-      if (addl > 0)
-      {
-        add = (mul[k] - '0') + addl;
-        if (add > 9)
-          mul[k - 1] = (add / 10) + '0';
-        mul[k] = (add % 10) + '0';
-      }
-      i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
-    }
-    if (j < 0)
-    {
-      if (mul[0] != '0')
-        break;
-      lnout--;
-      free(mul), mul = malloc(lnout + 1), mul = _initialize_array(mul, lnout);
-      k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
-    }
-    if (j >= 0)
-    {
-      add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (mul[k] - '0') + addl;
-      addl = add / 10, mul[k] = (add % 10) + '0';
-    }
-  }
-  printf("%s\n", mul);
-  return (0);
+	}
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
 }
