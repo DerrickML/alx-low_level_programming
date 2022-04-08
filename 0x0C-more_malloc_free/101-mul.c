c4f67d157da1a6d5e7c43058a3f4d361f06782d2
DeroMal <derrickmal123@gmail.com>
2022-04-08 04:03:58 +0300

    what a hard one

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,30 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
-* main - multiply 2 numbers as arguments
-* @argc: number of arguments
-* @argv: arguments
-* Return: 0 success
-*/
-int main(int num1, char *num2[])
+ * is_digit - checks for digits
+ * @c: input character to check for digit
+ * Return: 0 failure, 1 success
+ */
+int is_digit(char c)
 {
-unsigned long mul;
-int i, j;
+	if (c >= '0' && c <= '9')
+		return (1);
+	printf("Error\n");
+	return (98);
+}
+/**
+ * multiply - multiplies 2 #'s, prints result, must be 2 #'s
+ * @num1: factor # 1 (1st number)
+ * @num2: factor # 2 (2nd number)
+ * Return: 0 fail, 1 success
+ */
+int *multiply(char *num1, char *num2)
+/**
+ * main - multiply 2 input #'s and print result or print Error
+ * @argc: input count of args
+ * @argv: input array of string args
+ * Return: 0, Success
+ */
+int main(int argc, char **argv)
+{
+	char *num1, *num2;
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
-	for (i = 1; i < num1; i++)
 	{
-		for (j = 0; num2[i][j] != '\0'; j++)
-		{
-			if (num2[i][j] > 57 || num2[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
-		}
-
+		printf("Error\n");
+		exit(98);
+		num1 = argv[1], num2 = argv[2];
+	else
+	{
+		num1 = argv[2], num2 = argv[1];
 	}
-	mul = atol(num2[1]) *  atol(num2[2]);
-	printf("%lu\n", mul);
-return (0);
+	sum_result = multiply(num1 * num2);
+	if (sum_result == NULL)
+		exit(98);
+	print_me(sum_result);
+	return (0);
 }
