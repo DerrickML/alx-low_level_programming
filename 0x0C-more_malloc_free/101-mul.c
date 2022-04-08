9b43e9e1c7c3a340abc77c00d9b7c0dcd57aab79
DeroMal <derrickmal123@gmail.com>
2022-04-08 03:14:34 +0300

    a program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -83,17 +83,17 @@ int _checknum(char *argv[], int n)
  */
 int main(int argc, char *argv[])
 {
-	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	int num1, num2, mu1, add, addl, i, j, k, ca;
 	char *nout;
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
-	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	num1 = _checknum(argv, 1), num2 = _checknum(argv, 2);
+	_is_zero(argv), mu1 = num1 + num2, nout = malloc(mu1 + 1);
 	if (nout == NULL)
 		printf("Error\n"), exit(98);
-	nout = _initialize_array(nout, lnout);
-	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	nout = _initialize_array(nout, mu1);
+	k = mu1 - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
 	{
 		if (i < 0)
@@ -105,15 +105,15 @@ int main(int argc, char *argv[])
 					nout[k - 1] = (add / 10) + '0';
 				nout[k] = (add % 10) + '0';
 			}
-			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+			i = ln1 - 1, j--, addl = 0, ca++, k = mu1 - (1 + ca);
 		}
 		if (j < 0)
 		{
 			if (nout[0] != '0')
 				break;
 			lnout--;
-			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+			free(nout), nout = malloc(mu1 + 1), nout = _initialize_array(nout, mu1);
+			k = mu1 - 1, i = num1 - 1, j = num2 - 1, ca = addl = 0;
 		}
 		if (j >= 0)
 		{
@@ -123,4 +123,4 @@ int main(int argc, char *argv[])
 	}
 	printf("%s\n", nout);
 	return (0);
-}
+
